83e94c6fe8074c0e8d3ecb7a0b73abf71256a5ae
Artem <temka199266@gmail.com>
2022-09-14 11:46:38 +0500

    Add Favorite User

---
+++ Examples005_FavoriteUser/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введи имя юзера");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "artem")
+{
+    Console.WriteLine("Ух это Artem!!!");
+}
+else
+{
+    Console.Write("Привет ,");
+    Console.WriteLine(username);
+}
