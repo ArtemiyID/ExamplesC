1a64b8a43eb5aabf2f73c83dc669b3c9561fcba7
Artem <temka199266@gmail.com>
2022-09-30 14:49:29 +0500

    Massiv_Primeri-String and int

---
+++ Work_C#/Ucheba_02.cs
@@ -0,0 +1,6 @@
+Console.Clear();
+int[] numbers = {1, 2, 3, 4, 5};
+string[] name = {"Anna", "Bob", "Artur", "Artem", "George"};
+    for (int i =0; i < 5; i++)
+Console.WriteLine($"{i} элемент - это {numbers[i]}!!!");
+// СВЕРХУ БЫЛ ПРИМЕР МАССИВА STRING ДАННЫХ С ИМЕНАМИ !!!!\
