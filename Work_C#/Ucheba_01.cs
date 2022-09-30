860fd0380d4d07c34b448e5e0d1c3933358b52c1
Artem <temka199266@gmail.com>
2022-09-30 14:03:20 +0500

    Massiv_Primeri-String and int

---
+++ Work_C#/Ucheba_01.cs
@@ -0,0 +1,6 @@
+﻿// ВЫВОДИМ ЭЛЕМЕНТ МАССИВА (STRING -текстовый-наши имена в примере... , int цифры -числовой)
+Console.Clear();
+int[] numbers = {1,2,3,4,5,6,7,8};
+string[] name = {"Bob","Kris","Artem","Anna","John"};
+Console.Write($"{name[2]} !!!");
+Console.Write($"{numbers[3]} !!!");
