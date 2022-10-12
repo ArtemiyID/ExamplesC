f0f9c09a39abc72b1124c754e732f35fd6358985
Artem <temka199266@gmail.com>
2022-10-12 22:44:29 +0500

    Tochki Peresecheniya_43

--- Work/work_02.cs
+++ Work/work_02.cs
@@ -1,62 +1,15 @@
-Console.Write("Введи числа через запятую: ");
-int[] numbers = StringToNum(Console.ReadLine());
-PrintArray(numbers);
-int sum = 0;
-for (int i = 0; i < numbers.Length; i++)
-{
-    if (numbers[i] > 0)
-    {
-        sum++;
-    }
-}
-Console.WriteLine();
-Console.WriteLine($"количество значений больше 0 = {sum}");
+Console.WriteLine("введи значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введи число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введи значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введи число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
 
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
 
-int[] StringToNum(string input)
-{
-    int count = 1;
-    for (int i = 0; i < input.Length; i++)
-    {
-        if (input[i] == ',')
-        {
-            count++;
-        }
-    }
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
 
-    int[] numbers = new int [count];
-    int index = 0;
-
-    for (int i = 0; i < input.Length; i++)
-    {
-        string temp = "";
-
-        while (input [i] != ',')
-        {
-        if(i != input.Length - 1)
-        {
-            temp += input [i].ToString();
-            i++;
-        }
-        else
-        {
-            temp += input [i].ToString();
-            break;
-        }
-        }
-        numbers[index] = Convert.ToInt32(temp);
-        index++;
-    }
-    return numbers;
-}
-
-
-void PrintArray(int[] array)
-{
-    Console.Write("[ ");
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.Write("]");
-}
+ 
